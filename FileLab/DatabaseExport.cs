f93a3f4c43d13abace4b6fd9c470db670738e573
Nev River <huydnevermore@gmail.com>
2021-03-11 09:50:04 +0700

    Updated Data source

--- FileLab/DatabaseExport.cs
+++ FileLab/DatabaseExport.cs
@@ -28,7 +28,7 @@ namespace FileLab
 
             try
             {
-                using (SqlConnection connection = new SqlConnection("Data Source = enora; Initial Catalog = QLBH; User ID = test; Password = test"))
+                using (SqlConnection connection = new SqlConnection("Data Source = nr; Initial Catalog = QLBH; User ID = test; Password = test"))
                 {
                     connection.Open();
                     DataTable dataTable = new DataTable();
